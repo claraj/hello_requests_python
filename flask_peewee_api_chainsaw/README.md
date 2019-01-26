abbed908a2d6252c86a5f2064bf70f808502a09c
clara <clara@minneapolis.edu>
2019-01-26 11:26:31 -0600

    comments for api, requirements.txt for api

---
+++ flask_peewee_api_chainsaw/README.md
@@ -0,0 +1,19 @@
+## Flask and peewee app
+
+To run this app, set an environment variable 
+
+`FLASK_APP=app.py`
+
+Install dependencies 
+
+`pip install -r requirements.txt`
+
+Then run with 
+
+`flask run`
+
+
+
+
+
+
--- flask_peewee_api_chainsaw/app.py
+++ flask_peewee_api_chainsaw/app.py
@@ -3,15 +3,18 @@ from peewee import *
 from playhouse.shortcuts import model_to_dict
 
 
+# Configuration - database file name
 DATABASE = 'chainsaw.db'
+# Configuration - debug mode, see stack traces for errors
 DEBUG = True
 
-
+# Create a Flask web app
 app = Flask(__name__)
 
+# Connect to the database
 database = SqliteDatabase(DATABASE)
 
-
+# Model class, fields will map to columns in the database
 class Chainsaw(Model):
     name = CharField()
     country = CharField()
@@ -21,34 +24,50 @@ class Chainsaw(Model):
         database = database
 
 
+# Create table for model - just one table in this example
 database.create_tables([Chainsaw])
 
+
+# Before every request is handled, connect to the database
+# g is the application context for the lifetime of one request
+# app.before_request decorated functions are automatically called
+# before a request-handling function runs
 @app.before_request
 def before_request():
     g.db = database
     g.db.connect()
 
 
+# And close the DB connection after the request. This function
+# is passed the response and can modify it if needed. Here the
+# response is returned as-is.
 @app.after_request
 def after_request(response):
     g.db.close()
     return response
 
 
+# GET all the records
 @app.route('/api/chainsaw')
 def get_all():
+
+    # Get all the records from the database.
     res = Chainsaw.select()
-    for c in res:
-        print(c)
+    # Convert each record to a dictionary
+    # Convert this list of dictionaries to JSON
     return jsonify([model_to_dict(c) for c in res])
 
 
-@app.route('/api/chainsaw/<id>/')
-def get_by_id(id):
-    c = Chainsaw.get_by_id(id)
+# GET one record by ID
+@app.route('/api/chainsaw/<catcher_id>/')
+def get_by_id(catcher_id):
+    c = Chainsaw.get_by_id(catcher_id)
+    # Convert object into dictionary of fields and values;
+    # then convert the dictionary to JSON
     return jsonify(model_to_dict(c))
 
 
+# POST to create a new record
 @app.route('/api/chainsaw/', methods=['POST'])
 def add_new():
     with database.atomic():
@@ -57,26 +76,28 @@ def add_new():
             country=request.form['country'],
             catches=request.form['catches']
         )
-        return 'ok', 201
+        return 'ok', 201  # 201 status means 'resource created'
 
 
-@app.route('/api/chainsaw/<id>/', methods=['PATCH'])
-def update_chainsaw(id):
-    print(request.form)
+# PATCH to modify an existing record
+@app.route('/api/chainsaw/<catcher_id>/', methods=['PATCH'])
+def update_chainsaw(catcher_id):
     with database.atomic():
         Chainsaw.update(name=request.form['name'],
                         country=request.form['country'],
                         catches=request.form['catches'])\
-            .where(Chainsaw.id == id).execute()
-        return 'ok', 200
+            .where(Chainsaw.id == catcher_id).execute()
+        return 'ok', 200  # 200 status means 'ok', request successful
 
 
-@app.route('/api/chainsaw/<id>/', methods=['DELETE'])
-def delete_chainsaw(id):
+# DELETE to delete an existing record
+@app.route('/api/chainsaw/<catcher_id>/', methods=['DELETE'])
+def delete_chainsaw(catcher_id):
     with database.atomic():
-        Chainsaw.delete().where(Chainsaw.id == id).execute()
-    return 'ok', 200
+        Chainsaw.delete().where(Chainsaw.id == catcher_id).execute()
+    return 'ok', 200  # 200 status means 'ok', request successful
 
 
+# Start the web app running. By default, port 5000
 if __name__ == '__main__':
     app.run()
